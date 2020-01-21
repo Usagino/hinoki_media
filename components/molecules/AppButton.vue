b3b77543a5d28313928cf0b0927115c7bf003497
yuki <yuki.itoh@front-artgraph.com>
2020-01-21 15:22:49 +0900

    fix:carousel_style

---
+++ components/molecules/AppButton.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+.appbutton
+  p.text {{text}}
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: 'TEXT'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.appbutton {
+  background: $color-textcolorblack;
+  display: inline-block;
+  padding: 4px 36px;
+  cursor: pointer;
+  border-radius: 100vh;
+  margin: auto;
+  .text {
+    @include font-cardtitle;
+    color: $color-textcolorwhite;
+  }
+}
+</style>
