f5397aa3581f36c8b4317a78fa1d361979ceec35
yuki <yuki.itoh@front-artgraph.com>
2020-07-14 15:28:50 +0900

    update nuxt2.13

--- plugins/carousel.js
+++ plugins/carousel.js
@@ -1,4 +1,12 @@
 import Vue from 'vue'
 import VueCarousel from 'vue-carousel'
+import Carousel from 'vue-carousel/src/Carousel.vue'
+import Slide from 'vue-carousel/src/Slide.vue'
+import VuePaginate from 'vue-paginate'
 
 Vue.use(VueCarousel)
+Vue.component('Carousel', Carousel)
+Vue.component('Slide', Slide)
+
+// library
+Vue.use(VuePaginate)
